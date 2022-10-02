045d1a042ba75b8c72843661a4c0f9ef45bde5eb
jacob-stein1 <jmstein@bu.edu>
2022-10-01 20:52:18 -0400

    added support for video uploading

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images.app.goo.gl", "lh3.googleusercontent.com"],
+    domains: ["imgur.com", "lh3.googleusercontent.com"],
   },
 };
 
